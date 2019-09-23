ef7e271dee64323488158410c6868f44097c845f
Christian Campana-Emard <christian.campana.emard@outlook.com>
2019-09-23 17:11:55 -0400

    refactored to use new Azure Functions

---
+++ UCFParkingBot.Twitter/TweetSpotsAvailable.cs
@@ -0,0 +1,44 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using UCFParkingBot.Library;
+
+namespace UCFParkingBot.Twitter
+{
+    public static class TweetSpotsAvailable
+    {
+        [FunctionName("TweetSpotsAvailable")]
+        public static void Run([TimerTrigger("0 */15 * * * 1-5")]TimerInfo myTimer, ILogger log)
+        {
+            ParkingDataFunctions.SetParkingData();
+            string parkingData = ParkingDataFunctions.ParkingDataAsString();
+            
+            // Log the full output of the parking data
+            log.LogInformation($"{parkingData}");
+
+            Garage leastAvailableGarage = ParkingDataFunctions.GetLeastAvailableGarageBySpots();
+            //If there are fewer than 100 spots left, tweet!
+            if (leastAvailableGarage.SpotsAvailable < 100)
+            {
+                //get Twitter API keys from Key Vault and authenticate with Twitter
+                TwitterHelper.LoginToTwitter();
+
+                bool isTweetPublished = TwitterHelper.PublishTweet(parkingData);
+                if (isTweetPublished)
+                {
+                    log.LogInformation($"Tweet published at {DateTime.UtcNow} UTC!");
+                    TwitterHelper.CleanUpTimeline(3);
+                }
+                else
+                {
+                    log.LogInformation($"Tweet could not be published at {DateTime.UtcNow} UTC!");
+                }
+            }
+            else
+            {
+                log.LogInformation($"Tweet not published at {DateTime.UtcNow} UTC. Garage with least availability: {leastAvailableGarage.Name} with {leastAvailableGarage.SpotsAvailable} spots free.");
+            }
+        }
+    }
+}
